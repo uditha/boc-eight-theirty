2aab84159a59001b0fdf9428429fac0c516c550c
Uditha Tennakoon <uditha13@gmail.com>
2025-03-02 20:52:12 +0530

    udimax started

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -6,7 +6,7 @@ export default function Welcome() {
 
     return (
         <>
-            <Head title="Welcome">
+            <Head title="Udimaxweb">
                 <link rel="preconnect" href="https://fonts.bunny.net" />
                 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
             </Head>
